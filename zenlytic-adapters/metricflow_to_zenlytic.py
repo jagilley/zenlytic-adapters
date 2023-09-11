4a3f2c1249210224fd433c4b1afd0e40abab6180
Jasper <jaspergilley2021@u.northwestern.edu>
2023-09-11 12:44:30 -0700

    add a bit of dimensions

--- zenlytic-adapters/metricflow_to_zenlytic.py
+++ zenlytic-adapters/metricflow_to_zenlytic.py
@@ -6,20 +6,38 @@ with open("zenlytic-adapters/examples/metricflow/revenue.yml", 'r') as stream:
     except yaml.YAMLError as exc:
         print(exc)
 
-print(yaml_data)
+# print(yaml_data)
 # {'semantic_models': [{'name': 'orders', 'description': 'A model containing order data. The grain of the table is the order id.\n', 
 
 map = {
     "semantic_models.name": "name",
     "semantic_models.description": "description",
+    "semantic_models.model": None,
 }
 
-zenlytic_data = {}
+zenlytic_data = {
+    "fields": [],
+}
 
 for key, value in map.items():
     keys = key.split(".")
     for semantic_model in yaml_data[keys[0]]:
-        if keys[1] in semantic_model:
+        if keys[1] in semantic_model and value is not None:
             zenlytic_data[value] = semantic_model[value]
 
+# handle dimensions
+for dimension in yaml_data["semantic_models"][0]["dimensions"]:
+    if "name" in dimension:
+        zenlytic_data["fields"].append({
+            "name": dimension["name"],
+            "field_type": "dimension",
+            "sql": dimension["expr"],
+            # "type": dimension["type"], # does not map 1:1
+        })
+
 print(zenlytic_data)
+
+# convert zenlytic data to yaml
+zenlytic_yaml = yaml.dump(zenlytic_data)
+
+# print(zenlytic_yaml)
